b4b5253eac9785e339db795938f237648988e1e2
m34wzaa <daaaavethemagicalcheesewizard@gmail.com>
2026-09-11 22:48:24 +0000

    accidentally made 1268 files help

---
+++ main.js
@@ -0,0 +1,61 @@
+import * as THREE from './node_modules/three/build/three.module.js'
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+scene.background = new THREE.Color(0x222222);
+const lights = [new THREE.DirectionalLight(0xffffff, 1), new THREE.DirectionalLight(0xffffff, 1), new THREE.AmbientLight(0x404040)];
+lights[0].position.set(1, 1, 1).normalize();
+lights[1].position.set(-1, -1, -1).normalize();
+scene.add(lights[0]);
+scene.add(lights[1]);
+scene.add(lights[2]);
+const potLights = [new THREE.CylinderGeometry(0.1, 0.1, 0.5, 32), new THREE.MeshStandardMaterial({ color: 0xffff00, metalness: 1, roughness: 0.5 })];   
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshStandardMaterial({ color: 0xffffff, metalness: 0.5, roughness: 0.5 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+let keys = []
+document.addEventListener('keydown', (e) => {
+    keys[e.key] = true;
+});
+document.addEventListener('keyup', (e) => {
+    keys[e.key] = false;
+});
+document.addEventListener('click', () => {
+    document.body.requestPointerLock();
+});
+document.addEventListener('mousemove', (e) => {
+    if (document.pointerLockElement === document.body) {
+        camera.rotation.order = 'YXZ';
+        camera.rotation.y -= e.movementX * 0.002 * sens;
+        camera.rotation.x -= e.movementY * 0.002 * sens;
+        camera.rotation.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, camera.rotation.x));
+    }
+});
+let sens = 5;
+function animate() {
+    requestAnimationFrame(animate);
+    if (keys['w']) {
+        camera.position.add(new THREE.Vector3(0, 0, -0.1).applyQuaternion(camera.quaternion));
+    }
+    if(keys['s']) {
+        camera.position.add(new THREE.Vector3(0, 0, 0.1).applyQuaternion(camera.quaternion));
+    }
+    if(keys['a']) {
+        camera.position.add(new THREE.Vector3(-0.1, 0, 0).applyQuaternion(camera.quaternion));
+    }
+    if(keys['d']) {
+        camera.position.add(new THREE.Vector3(0.1, 0, 0).applyQuaternion(camera.quaternion));
+    }
+    if(keys['q']) {
+        camera.position.add(new THREE.Vector3(0, -0.1, 0).applyQuaternion(camera.quaternion));
+    }
+    if(keys['e']) {
+        camera.position.add(new THREE.Vector3(0, 0.1, 0).applyQuaternion(camera.quaternion));
+    }
+    renderer.render(scene, camera);
+}
+animate();
